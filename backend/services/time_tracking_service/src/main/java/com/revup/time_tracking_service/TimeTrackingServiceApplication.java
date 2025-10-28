0f19be2606e239bde9bd2cf8a5fd7f0a3926ecc5
CHAMILKA MIHIRAJ <120358764+ChamilkaMihiraj2002@users.noreply.github.com>
2025-10-28 22:16:43 +0530

    Update TimeTrackingServiceApplication.java

--- backend/services/time_tracking_service/src/main/java/com/revup/time_tracking_service/TimeTrackingServiceApplication.java
+++ backend/services/time_tracking_service/src/main/java/com/revup/time_tracking_service/TimeTrackingServiceApplication.java
@@ -2,12 +2,14 @@ package com.revup.time_tracking_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TimeTrackingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimeTrackingServiceApplication.class, args);
+        SpringApplication.run(TimeTrackingServiceApplication.class, args);
 	}
 
 }
